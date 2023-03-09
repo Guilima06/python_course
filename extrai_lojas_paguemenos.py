df1e3b5443d3745db31bb1eefa5af38fbb09842f
guilherme.lima <guilhermelima96@outlook.com>
2023-03-09 19:39:28 -0300

    atividades atualizadas

--- extrai_lojas_paguemenos.py
+++ extrai_lojas_paguemenos.py
@@ -1,13 +1,19 @@
+from tkinter.filedialog import asksaveasfilename
+
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
 import tkinter as tk
 from tkinter import filedialog
 import openpyxl
+import re
 
 
 # Selecionando o arquivo com o código HTML
-file_path = '/home/guilherme/Dropbox/HTML/Lojas/paguemenos.html'
+# Cria uma janela de diálogo para selecionar o arquivo de entrada
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
 
 # Abrindo o arquivo e lendo o conteúdo
 with open(file_path, 'r', encoding='utf-8') as file:
@@ -15,6 +21,8 @@ with open(file_path, 'r', encoding='utf-8') as file:
 
 # Criando o objeto BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
+for br in soup.find_all('br'):
+    br.replace_with(', ')
 
 # Encontrando todas as lojas
 stores = soup.find_all('div', {'class': 'box_loja'})
@@ -22,23 +30,28 @@ stores = soup.find_all('div', {'class': 'box_loja'})
 
 # Criando uma lista para armazenar as informações
 store_list = []
-
-# Iterando sobre todas as lojas encontradas
-for index, store in enumerate(stores):
+#
+# # Iterando sobre todas as lojas encontradas
+for store in stores:
     # Verificando se a tag com a classe desejada existe
     name_element = store.find('h3', {'class': 'title_loja'})
     if name_element is not None:
         # Extraindo as informações
         name = name_element.text.strip().title()
-        address = store.find('li').text.strip().replace('\n                 ','')
+        address = store.find('li').text.strip().replace('\n                  ','')
         dados_loja = {
             'Nome': name,
-            'Endereço': address,
+            'Endereço': address
         }
         store_list.append(dados_loja)
 print(store_list)
 # Criando um dataframe do pandas com as informações coletadas
 df = pd.DataFrame(store_list, columns=['Nome', 'Endereço'])
 
+# abre a caixa de diálogo para seleção do local de salvamento do arquivo Excel
+filename = tk.filedialog.asksaveasfilename(title='Salvar arquivo Excel', filetypes=[('Excel files', '*.xlsx')])
+if not filename.endswith('.xlsx'):
+    filename += '.xlsx'
+
 # Salvando o dataframe em um arquivo Excel
-df.to_excel('/home/guilherme/Dropbox/HTML/Lojas/Stores.xlsx', index=False)
+df.to_excel(filename)
